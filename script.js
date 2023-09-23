59d10621b2fe6bf2aa0c1ee5e38bdd035bcac7a6
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2023-09-23 20:32:49 +0300

    initial addTask function

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const inputField = document.getElementById('input');
 const taskList = document.getElementById('task-list');
 
+const savedTodos = JSON.parse(localStorage.getItem('tasks')) || [];
+savedTodos.forEach(task => addTask(task));
 inputField.addEventListener('submit', function(e) {
 
     e.preventDefault();
+
     addTask(e.target.elements["input-field"].value);
 })
 
 const addTask = (task) => {
-    console.log(task);
+
+    const tasks = localStorage.getItem('tasks') || [];
+
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    td.textContent = task;
+    tr.appendChild(td);
+    taskList.appendChild(tr);
+
 }
+
+
+
+function saveTodoToLocal(task) {
+    const todos = JSON.parse(localStorage.getItem('tasks')) || [];
+    todos.push(task);
+    localStorage.setItem('tasks', JSON.stringify(todos));
+}
+
