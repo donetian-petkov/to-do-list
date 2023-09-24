faaf2cd830b6e91b314776a6f29eb3b56b237c32
donetian-petkov <donetianpetkov@gmail.com>
2023-09-24 11:29:12 +0300

    Working Adding Task

--- script.js
+++ script.js
@@ -6,31 +6,41 @@ inputField.addEventListener('submit', function(e) {
 
     const task = e.target.elements["input-field"].value;
 
-    addTask(task);
-    saveTodoToLocal(task)
+    addTask(task, true);
 })
 
-const addTask = (task) => {
+const addTask = (task, save = false) => {
 
-    console.log(task);
+    const tasksString = localStorage.getItem('tasks');
+    const tasks = tasksString ? JSON.parse(tasksString) : [];
 
-    const tasks = localStorage.getItem('tasks') || [];
 
     const tr = document.createElement('tr');
     const id = document.createElement('td');
     const todo = document.createElement('td');
 
-    id.textContent = tasks.length.toString();
-    todo.textContent = task;
+    id.textContent = task.id || (tasks.length + 1).toString();
+    todo.textContent = task.taskInput || task;
 
     tr.appendChild(id);
     tr.appendChild(todo);
     taskList.appendChild(tr);
 
+    if (save) {
+        saveTodoToLocal(task);
+    }
+
 }
 
-function saveTodoToLocal(task) {
+function saveTodoToLocal(taskInput) {
     const todos = JSON.parse(localStorage.getItem('tasks')) || [];
+    const id = todos.length + 1;
+
+    const task = {
+        id,
+        taskInput
+    }
+
     todos.push(task);
     localStorage.setItem('tasks', JSON.stringify(todos));
 }
