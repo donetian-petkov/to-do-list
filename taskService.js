f0eeeddbfa7a6f3d08db4a2a0eb7140de63c11e8
donetian-petkov <donetianpetkov@gmail.com>
2023-09-25 14:34:46 +0300

    Change Status Task

--- taskService.js
+++ taskService.js
@@ -1,4 +1,4 @@
-export const addTask = (task, HTMLElem, save = false) => {
+export const addTask = (task, HTMLElem, saveTask = false) => {
 
     const tasksString = localStorage.getItem('tasks');
     const tasks = tasksString ? JSON.parse(tasksString) : [];
@@ -20,11 +20,16 @@ export const addTask = (task, HTMLElem, save = false) => {
     status.textContent = task.status || 'Not Completed';
     tr.id = `task-list-${task.id || (tasks.length + 1)}`;
 
-    deleteButton.addEventListener('click', function() {
+    status.addEventListener('click', function () {
 
-        deleteTask(task.id || (tasks.length + 1),HTMLElem);
+        changeTaskStatus(task.id || (tasks.length + 1), HTMLElem);
 
-    })
+    });
+    deleteButton.addEventListener('click', function () {
+
+        deleteTask(task.id || (tasks.length + 1), HTMLElem);
+
+    });
 
     deleteButton.appendChild(icon);
     tr.appendChild(id);
@@ -33,35 +38,49 @@ export const addTask = (task, HTMLElem, save = false) => {
     tr.appendChild(deleteButton);
     HTMLElem.appendChild(tr);
 
-    if (save) {
+    if (saveTask) {
         saveTaskToLocalStorage(task);
     }
 
 }
 
-export function deleteTask(id, taskList) {
+export function changeTaskStatus(id, HTMLElem) {
+
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const newTasks = savedTasks.map(x => {
+
+            if (x.id === id) {
+                return {
+                    ...x,
+                    status: x.status === 'Completed' ? 'Not Completed' : 'Completed'
+                }
+            } else return x;
+
+        }
+    );
+
+  updateTaskList(newTasks,HTMLElem);
+
+
+}
+
+export function deleteTask(id, HTMLElem) {
 
     const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
     const newTasks = savedTasks.filter(x => x.id !== id).map(x =>
-        x.id > id
-            ? { ...x, id:x.id-1 }
+        x.id >= id
+            ? {...x, id: x.id - 1}
             : x
     );
 
-    localStorage.removeItem('tasks');
-    localStorage.setItem('tasks', JSON.stringify(newTasks));
-
-    taskList.replaceChildren();
-    newTasks.forEach(task => addTask(task,taskList));
+    updateTaskList(newTasks,HTMLElem);
 
 }
 
 export function saveTaskToLocalStorage(taskInput) {
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     const id = tasks.length + 1;
-
     const status = 'Not Completed';
-
     const task = {
         id,
         taskInput,
@@ -72,4 +91,11 @@ export function saveTaskToLocalStorage(taskInput) {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+export function updateTaskList(tasks,HTMLElem) {
 
+    localStorage.removeItem('tasks');
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    HTMLElem.replaceChildren();
+    tasks.forEach(task => addTask(task, HTMLElem));
+}
